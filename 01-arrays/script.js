9077b4ecbc45f80830e7091cb4198553c63eeb2e
leonardo-monti <monti.leonardo22@gmail.com>
2025-12-18 14:10:57 +0100

    empty

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -50,4 +50,6 @@ const lewisIndex = null;
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty)
